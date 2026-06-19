395853645fec1682da266116541308430ab5996d
achmad rivai <115203286+rivaiRBotich@users.noreply.github.com>
2026-06-19 22:41:42 +0700

    tambah clinic category tabel user

--- database/migrations/2026_06_19_152935_add_clinic_category.php
+++ database/migrations/2026_06_19_152935_add_clinic_category.php
@@ -9,12 +9,10 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_online')->default(false);
-            $table->timestamp('last_seen_at')->nullable();
-            $table->string('clinic_category');
+            $table->string('clinic_category')->nullable();
         });
     }
 
@@ -24,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('clinic_category');
         });
     }
 };
