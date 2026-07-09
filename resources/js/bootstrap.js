e6bf454d683a3a7d7f0a45c5cf46eaaa6c622e5e
Chandra <Chandra888lim@gmail.com>
2026-07-09 20:39:39 +0700

    test fix bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,9 +6,9 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: '127.0.0.1', // <-- Kita kunci mati ke IP localhost agar instan terhubung
+    wsHost: import.meta.env.VITE_REVERB_HOST, // <-- Kita kunci mati ke IP localhost agar instan terhubung
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+    wssPort: 443,
     forceTLS: true,     // <-- Wajib false karena di localhost kita menggunakan HTTP biasa, bukan HTTPS
     enabledTransports: ['ws', 'wss'],
     authEndpoint: '/broadcasting/auth', // ← WAJIB ADA
